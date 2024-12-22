aed659639f2b300b93ebe4c7a596f2452e6b87b6
SEANGLAY <devolutionary.wizard@gmail.com>
2024-12-22 16:36:50 +0700

    initial project

---
+++ src/main/kotlin/com/seanglay/library_sys/LibrarySysApplication.kt
@@ -0,0 +1,11 @@
+package com.seanglay.library_sys
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibrarySysApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibrarySysApplication>(*args)
+}
